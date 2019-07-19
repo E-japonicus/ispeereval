58645ec1b150ad22fe41d3fc1696c9e7346b17f0
Masaki Sato <e.japonicus@gmail.com>
2019-07-19 10:19:11 +0900

    20190719 v3.0 #fix

--- teachers_view.php
+++ teachers_view.php
@@ -1,16 +1,23 @@
 <link rel="stylesheet" type="text/css" href="./style.css">
 
-<ul class="nav nav-tabs">
-  <li class="active"><a data-toggle="tab" href="#form">フォーム</a></li>
-  <li><a data-toggle="tab" href="#list">ルーブリック登録一覧</a></li>
-</ul>
-
+<!-- タブボタン部分 -->
+<div class="tab-button">
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a href="#form" class="nav-link active" data-toggle="tab">フォーム</a>
+    </li>
+    <li class="nav-item">
+      <a href="#list" class="nav-link" data-toggle="tab">登録一覧</a>
+    </li>
+  </ul>
+</div>
+  
+<!--タブのコンテンツ部分-->
 <div class="tab-content">
-  <div id="form" class="tab-pane fade fade in active">
+  <div id="form" class="tab-pane active">
     <?php require_once("{$CFG->dirroot}/mod/ispeereval/peereval_form.php"); ?>
   </div>
-
-  <div id="list" class="tab-pane fade">
+  <div id="list" class="tab-pane">
     <?php require_once("{$CFG->dirroot}/mod/ispeereval/teachers_list.php"); ?>
   </div>
 </div>
--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ispeereval';
-$plugin->version = 2019070302;
+$plugin->version = 2019071901;
 $plugin->release = 'v3.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
